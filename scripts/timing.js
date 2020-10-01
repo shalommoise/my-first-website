ad812526a8cd7764d512dd18cd6cdd2e16b2bbac
Shalommoise <mshalom689@gmail.com>
2020-10-01 23:49:16 +0100

    merged timer &  greeting to 1

--- scripts/timing.js
+++ scripts/timing.js
@@ -3,7 +3,15 @@ const timing = () => {
   const takeOut = myDate.split("G")[0].split(" ")[4];
   return takeOut;
 };
+const whatTimeIsit = () => {
+  let time = Number(Date().split(" ")[4].split(":")[0]);
 
+  if (time >= 0 && time < 12) return "Good Morning";
+  else if (time >= 12 && time < 17) return "Good Afternoon";
+  else if (time >= 17 && time < 23) return "Good Evening";
+};
 setInterval(function () {
-  document.getElementById("time").innerHTML = timing();
+  document.getElementById(
+    "time"
+  ).innerHTML = `${whatTimeIsit()}. Current Time: ${timing()}`;
 }, 1000);
