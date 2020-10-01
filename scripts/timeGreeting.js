a865d3b064a76a3bf59450bf30aef15c21a33406
Shalommoise <mshalom689@gmail.com>
2020-10-01 23:50:24 +0100

    added to footer

--- scripts/timeGreeting.js
+++
@@ -1,8 +0,0 @@
-const whatTimeIsit = () => {
-  let time = Number(Date().split(" ")[4].split(":")[0]);
-
-  if (time >= 0 && time < 12) return "Good Morning";
-  else if (time >= 12 && time < 17) return "Good Afternoon";
-  else if (time >= 17 && time < 23) return "Good Evening";
-};
-document.getElementById("greeting").innerHTML = whatTimeIsit();
