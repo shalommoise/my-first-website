daa7e55b5102a56afb117e4d8310534125e24850
Shalommoise <mshalom689@gmail.com>
2020-10-02 17:08:07 +0100

    full date

--- scripts/timing.js
+++ scripts/timing.js
@@ -10,8 +10,32 @@ const whatTimeIsit = () => {
   else if (time >= 12 && time < 17) return "Good Afternoon";
   else if (time >= 17 && time < 23) return "Good Evening";
 };
+const getDate = () => {
+  const months = {
+    Jan: "January",
+    Feb: "Febuary",
+    Mar: "March",
+    Apr: "April",
+    May: "May",
+    Jun: "June",
+    Jul: "July",
+    Aug: "Aug",
+    Sep: "Spetmber",
+    Oct: "October",
+    Nov: "November",
+    Dec: "Dec",
+  };
+  const myDate = Date();
+  const day = myDate.split(" ")[0] + "day";
+
+  const date = `${myDate.split(" ")[0]}day ${myDate.split(" ")[2]} ${
+    months[myDate.split(" ")[1]]
+  } ${myDate.split(" ")[3]}`;
+  return date;
+};
+
 setInterval(function () {
   document.getElementById(
     "time"
-  ).innerHTML = `${whatTimeIsit()}. Current Time: ${timing()}`;
+  ).innerHTML = `${whatTimeIsit()}! The current time where you are is: ${timing()} ${getDate()}`;
 }, 1000);
