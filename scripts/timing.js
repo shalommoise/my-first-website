3741de98bf18749e1bfa82419be12a198e1ea1b8
Shalommoise <mshalom689@gmail.com>
2020-10-06 00:38:34 +0100

    for all hours

--- scripts/timing.js
+++ scripts/timing.js
@@ -8,7 +8,8 @@ const whatTimeIsit = () => {
 
   if (time >= 0 && time < 12) return "Good Morning";
   else if (time >= 12 && time < 17) return "Good Afternoon";
-  else if (time >= 17 && time < 23) return "Good Evening";
+  else if (time >= 17 && time < 24) return "Good Evening";
+  else return "Hello";
 };
 const getDate = () => {
   const months = {
