c01a41ec0e34c4cbd553f34813bb6f15e794c8fd
Shalommoise <mshalom689@gmail.com>
2020-10-05 17:08:58 +0100

    starting on NC-news

--- scripts/website.js
+++ scripts/website.js
@@ -1,15 +1,14 @@
 const showWeb = () => {
   const web = document.getElementsByClassName("web")[0];
   const webButton = document.getElementsByClassName("webButton")[0];
-  document.getElementById("openNc").innerHTML =
-    "Click here to to open NC-news in a new browser";
+  const heading = document.getElementById("NC-newsH");
+
   if (web.style.display !== "block") {
     web.style.display = "block";
-
-    webButton.innerHTML = "Hide Website";
+    heading.innerHTML = "Hide NC-News";
   } else {
     web.style.display = "none";
-    webButton.innerHTML = "Show Website";
+    heading.innerHTML = "NC-News";
   }
 };
 
