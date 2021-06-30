bee189e0a05c8919f8751966fe5ac3f8fdb69da6
shalommoise <shalommoise@github.com>
2021-06-30 16:44:09 +0100

    fixed missing social media icons

--- scripts/insertComponents.js
+++ scripts/insertComponents.js
@@ -13,13 +13,13 @@ const socialMediaFooter =()=>{
         <a href="mailto:mshalom689@gmail.com?subject=Hi Shalom" target='_blank'><img class="footericon"
             src="https://image.flaticon.com/icons/png/512/8/8807.png" alt="email" /></a>
         <a href="https://www.facebook.com/shalom.moise" target='_blank'><img class="footericon"
-            src="https://www.iconfinder.com/data/icons/capsocial-round/500/facebook-512.png" alt="facebook" /></a>
+            src="./pictures/socials/facbook logo.png" alt="facebook" /></a>
       
         <a href="https://www.instagram.com/shalommoise/" target='_blank'><img class="footericon"
-            src="https://www.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-instagram-new-circle-512.png"
+            src="./pictures/socials/instagram logo.jpeg"
             alt="instagram" /></a>
               <a href="https://twitter.com/MoiseShalom" target='_blank'><img class="footericon"
-                  src="https://www.iconfinder.com/data/icons/logotypes/32/square-twitter-512.png" alt="twitter" /></a>`;
+                  src="./pictures/socials/twitter logo.jpeg" alt="twitter" /></a>`;
 
 
 body.append(footer);
