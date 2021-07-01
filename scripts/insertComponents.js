22bc17a228a80ac10254e48347803b9557f6d5be
shalommoise <shalommoise@github.com>
2021-07-01 14:47:59 +0100

    got nicer social icons

--- scripts/insertComponents.js
+++ scripts/insertComponents.js
@@ -3,23 +3,23 @@ const socialMediaFooter =()=>{
   const footer = document.createElement("footer");
   
   footer.innerHTML =  `<a href="https://github.com/shalommoise" target='_blank'>
-          <img class="footericon" src="https://cdn.iconscout.com/icon/free/png-256/github-153-675523.png" alt="GitHub" /></a>
+          <img class="footericon" src="./pictures/socials/git hub logo.png" alt="GitHub" /></a>
         <a href="https://www.linkedin.com/in/shalom-moise-20a294128/" target='_blank'>
-          <img class="footericon" src="https://www.vectorico.com/wp-content/uploads/2018/02/LinkedIn-Icon-Square.png"
+          <img class="footericon" src="./pictures/socials/linkedin logo.png"
             alt="LinkedIn" /></a>
         <a href="https://www.strava.com/athletes/51107146" id="strava" target='_blank'><img class="footericon"
-            src="https://cdn.worldvectorlogo.com/logos/strava-2.svg" alt="Strava" /></a>
+            src="./pictures/socials/strava logo.svg" alt="Strava" /></a>
         
         <a href="mailto:mshalom689@gmail.com?subject=Hi Shalom" target='_blank'><img class="footericon"
-            src="https://image.flaticon.com/icons/png/512/8/8807.png" alt="email" /></a>
+            src="./pictures/socials/email logo.png" alt="email" /></a>
         <a href="https://www.facebook.com/shalom.moise" target='_blank'><img class="footericon"
             src="./pictures/socials/facbook logo.png" alt="facebook" /></a>
       
         <a href="https://www.instagram.com/shalommoise/" target='_blank'><img class="footericon"
-            src="./pictures/socials/instagram logo.jpeg"
+            src="./pictures/socials/instagram logo.png"
             alt="instagram" /></a>
               <a href="https://twitter.com/MoiseShalom" target='_blank'><img class="footericon"
-                  src="./pictures/socials/twitter logo.jpeg" alt="twitter" /></a>`;
+                  src="./pictures/socials/twitter logo.png" alt="twitter" /></a>`;
 
 
 body.append(footer);
