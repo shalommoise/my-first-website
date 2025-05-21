6631c54b42f102f9cf08bdbd3cea3a09398d7782
Shalom Moise <shalommoise@hotmail.co.uk>
2025-05-21 21:04:42 +0100

    refactor social footer links

--- scripts/insertComponents.js
+++ scripts/insertComponents.js
@@ -1,28 +1,24 @@
-const socialMediaFooter =()=>{
-  const body = document.querySelector('body'); 
+const makinglink = (link, porfileLink, alt) => {
+  const a = document.createElement("a");
+  const img = document.createElement("img");
+  a.href = link;
+  a.target = "_blank";
+  a.rel = "noopener noreferrer";
+  img.src = `./pictures/socials/${porfileLink}`;
+  img.alt = alt;
+  img.className = "footericon";
+  a.appendChild(img)
+  return a;
+}
+
+const socialMediaFooter = () => {
+  const body = document.querySelector('body');
   const footer = document.createElement("footer");
-  
-  footer.innerHTML =  `<a href="https://github.com/shalommoise" target='_blank'>
-          <img class="footericon" src="./pictures/socials/git hub logo.png" alt="GitHub" /></a>
-        <a href="https://www.linkedin.com/in/shalom-moise-20a294128/" target='_blank'>
-          <img class="footericon" src="./pictures/socials/linkedin logo.png"
-            alt="LinkedIn" /></a>
-        <a href="https://www.strava.com/athletes/51107146" id="strava" target='_blank'><img class="footericon"
-            src="./pictures/socials/strava logo.svg" alt="Strava" /></a>
-        
-        <a href="mailto:mshalom689@gmail.com?subject=Hi Shalom" target='_blank'><img class="footericon"
-            src="./pictures/socials/email logo.png" alt="email" /></a>
-        <a href="https://www.facebook.com/shalom.moise" target='_blank'><img class="footericon"
-            src="./pictures/socials/facbook logo.png" alt="facebook" /></a>
-      
-        <a href="https://www.instagram.com/shalommoise/" target='_blank'><img class="footericon"
-            src="./pictures/socials/instagram logo.png"
-            alt="instagram" /></a>
-              <a href="https://twitter.com/MoiseShalom" target='_blank'><img class="footericon"
-                  src="./pictures/socials/twitter logo.png" alt="twitter" /></a>`;
-
-
-body.append(footer);
+  footer.appendChild(makinglink("https://github.com/shalommoise", "git hub logo.png", "GitHub"));
+  footer.appendChild(makinglink("https://www.linkedin.com/in/shalom-moise-20a294128/", "linkedin logo.png", "LinkedIn"));
+  footer.appendChild(makinglink("https://www.strava.com/athletes/51107146", "strava logo.svg", "Strava"));
+  footer.appendChild(makinglink("mailto:mshalom689@gmail.com?subject=Hi Shalom", "email logo.png", "email"));
+  body.append(footer);
 }
 
 
